52ed532b788a30816021dde8690042b0450d0073
Vetrivel060306 <velravi060306@gmail.com>
2025-10-02 14:56:39 +0800

    medical_data_visualizer.py

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -19,34 +19,28 @@ df['gluc'] = np.where(df['gluc'] == 1, 0, 1)
 
 # 4
 def draw_cat_plot():
-    # 5
-    df_cat = df.melt(
-        id_vars=['cardio'],
-        value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight']
-    )
+    #5
+    df_cat = pd.melt(df, id_vars=['cardio'],
+                     value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
 
-
-    # 6
-    
-    
+    #6
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index()
+    df_cat = df_cat.rename(columns={0: 'total'})
 
     # 7
 
 
-
-
     # 8
-    fig = sns.catplot(
-        data=df_cat,
-        kind='count',
-        x='variable',
-        hue='value',
-        col='cardio',
-    ).set_axis_labels("Variable", "Total")
+    graph = sns.catplot(data=df_cat,
+                        kind="bar",
+                        x="variable",
+                        y="total",
+                        hue="value",
+                        col="cardio"
+    )
+    fig = graph.figure
 
-
-
-    # 9
+    #9
     fig.savefig('catplot.png')
     return fig
 
@@ -77,9 +71,7 @@ def draw_heat_map():
     fig, ax = plt.subplots(figsize=(12, 12))
 
     # 15
-    sns.heatmap(corr, annot=True, fmt=".1f", ax=ax, mask=mask, square=True, linewidths=.5, center=0)
-    ax.set_title('Correlation Heatmap')
-
+    sns.heatmap(corr, annot=True, fmt=".1f", ax=ax, mask=mask, square=True, linewidths=.5,center=0)
 
     # 16
     fig.savefig('heatmap.png')
